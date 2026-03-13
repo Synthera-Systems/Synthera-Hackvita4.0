9cf9b0a19e4d402ed8d8a73e60a8556e19f98dfc
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2026-03-14 01:52:49 +0530

    add hf space config

--- backend/src/routes/v1/route.js
+++ backend/src/routes/v1/route.js
@@ -4,6 +4,11 @@ import emergencyContactsRoutes from "./emergencyContacts.routes.js";
 import triggerRoute from "./trigger.route.js";
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the API v1"
+  });
+});
 router.use("/auth", authRoutes);
 router.use("/emergency-contacts", emergencyContactsRoutes);
 router.use("/trigger", triggerRoute);
